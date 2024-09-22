81e2136b4ebc0e1b805cf6560887ec63744c726a
azeembashir <azeembashir.33@gmail.com>
2024-09-22 10:09:07 +0500

    Initial commit

---
+++ app.js
@@ -0,0 +1,24 @@
+// with method
+// var string = prompt("Enter a string to check if it is a palindrome:");
+// var lower = string.toLowerCase();
+// var reveresed = lower.split('').reverse().join('');
+// if (lower === reveresed){
+//     alert(string + " is a Palindrome");
+// }else{
+//     alert(string + " is not a palindrome");
+// }
+
+// WITHOUT METHOD
+// var string = prompt("Enter a string to check if it is a palindrome:");
+// var lower = string.toLowerCase();
+// var reversedString = "";
+
+// for (var i = lower.length - 1; i >= 0; i--) {
+//     reversedString += lower[i];
+// }
+
+// if (lower === reversedString) {
+//     alert(string + " is a palindrome.");
+// } else {
+//     alert(string + " is not a palindrome.");
+// }
